7f20aadeac745ad1a8b1343eeff3b63e408e757c
soogary <38732384+soogary@users.noreply.github.com>
2024-05-06 21:28:19 +0000

    add files to repo

---
+++ hello.py
@@ -0,0 +1,11 @@
+#print("Hello world")
+
+my_list2 = ("apple", "cherry", "lemon")
+my_dict = {"drink":"coffee", "milk": "whole"}
+
+for item in my_list2:
+    print (f"my fave is eating: {item}")
+
+for _,value in my_dict.items():
+    print (f"my fave drink is: {value}")
+
