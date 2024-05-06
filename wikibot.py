03ac9832bf2d22d630aba42f04afb8e7aad5a6d8
soogary <38732384+soogary@users.noreply.github.com>
2024-05-06 23:42:18 +0000

    all

--- wikibot.py
+++ wikibot.py
@@ -1,12 +1,21 @@
-import wikipedia
-
 #wikipedia.summary("Facebook", sentences=1)
 #result = wikipedia.summary("Microsoft", sentences=1)
 #print(result)
 
+#====
+
+import wikipedia
+import click
+
+@click.command()
+@click.option('--name', prompt='your wikipedia page to scrape', help='web page you want to scrape')
+
 def scrape(name="Microsoft", length=1):
-    result = wikipedia.summary(name, sentences=1)
-    return result
+    result = wikipedia.summary(name, sentences=length)
+    click.echo(click.style(f"{result}:", bg="red", fg="blue"))
+
+if __name__ == '__main__':
+    scrape()
 
 #print(scrape())
 print(scrape("Microsoft"))
