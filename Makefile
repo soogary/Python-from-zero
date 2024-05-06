5b1e86d7248da2a0d4352e1ef0ff8c0e53e9b186
soogary <38732384+soogary@users.noreply.github.com>
2024-05-06 21:53:33 +0000

    add to repo

--- Makefile
+++ Makefile
@@ -1,1 +1,15 @@
+install:
+	pip install -r requirements.txt
 
+test:
+	python -m pytest -vv test_hello.py
+
+format:
+	black *.py
+   
+
+lint:
+
+    pylint --diable=R,C *.py
+
+all:  install lint test
--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 #print("Hello world")
 
-my_list2 = ("apple", "cherry", "lemon")
-my_dict = {"drink":"coffee", "milk": "whole"}
 
-for item in my_list2:
-    print (f"my fave is eating: {item}")
 
-for _,value in my_dict.items():
-    print (f"my fave drink is: {value}")
 
+#my_list2 = ("apple", "cherry", "lemon")
+#my_dict = {"drink":"coffee", "milk": "whole"}
+
+#for item in my_list2:
+#    print (f"my fave is eating: {item}")
+
+#for _,value in my_dict.items():
+#    print (f"my fave drink is: {value}")
+
+
+def add(x,y):
+    return x + y
+
+print (add(2,2))
