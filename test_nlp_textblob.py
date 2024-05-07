3d42adc137c2416caa04576c216b6df61bdd190d
soogary <38732384+soogary@users.noreply.github.com>
2024-05-07 20:40:11 +0000

    fine tune tests

--- test_nlp_textblob.py
+++ test_nlp_textblob.py
@@ -1,4 +1,7 @@
-from mylib.nlp_textblob import search_wikipedia, summarize_wikipedia, get_text_blob, get_phrases
+"""test"""
+#from mylib.nlp_textblob import search_wikipedia, summarize_wikipedia, get_text_blob, get_phrases
+from mylib.nlp_textblob import get_phrases
 
 def test_get_phrases():
+    """check sample values are present in the output"""
     assert 'nba' in get_phrases("Los Angeles Lakers")
